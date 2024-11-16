217629c2b7b2eaf3e054811ec63eccafefa8df42
mariegrace <mariegracebmg@gmail.com>
2024-11-16 05:50:02 +0200

    Add the team

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -3,6 +3,15 @@ import React, { useState, useEffect } from 'react';
 import hr from '../assets/abouthr.png';
 import Image from 'next/image';
 import lines from '../assets/linegroup.png';
+import bHr from '../assets/blackhr.png';
+import Kelly from '../assets/kelly.png';
+import Socrate from '../assets/socrate.png';
+import Djasmine from '../assets/Djasmine.png';
+import Henry from '../assets/henry.png';
+import { FaLinkedinIn } from "react-icons/fa";
+import { GrFacebookOption } from "react-icons/gr";
+import ig from '../assets/instagram.png';
+import Link from 'next/link';
 
 function TypingEffect() {
   const text = "Learn the story behind LK Recruitment";
@@ -23,7 +32,7 @@ function TypingEffect() {
 
   return (
     <div className="">
-      <div className='bg-[#F5F5FF] p-32'>
+      <div className='bg-[#F5F5FF] p-32 pb-14'>
       <h1 className="text-7xl mx-auto w-[70%] text-center">
         {displayedText}
         <span className="border-r-2 border-black animate-blink">&nbsp;</span>
@@ -43,6 +52,73 @@ function TypingEffect() {
       </div>
       </div>
     <Image src={lines} width={100} height={50} alt='lines' className=' mx-auto -mt-24 z-10 object-cover'/>
+      </div>
+
+      <div className='bg-white flex flex-col gap-10 justify-center text-center my-28'>
+        <h1 className='uppercase font-semibold text-2xl'>our amazing team</h1>
+        <Image src={bHr} width={200} height={1} alt='black hr' className='mx-auto -mt-7'/>
+        <p className='w-[30%] mx-auto font-light text-[15px]'>Ready to take the next step in your career? Explore our diverse range of available jobs and find 
+          the perfect opportunity that aligns with your skills and aspirations</p>
+          <div className='flex gap-7 justify-center'>
+ 
+  <div className="relative group">
+    <Image src={Kelly} width={250} height={100} alt="kelly" className="object-cover transition-all duration-300 group-hover:opacity-50" />
+    <div className="absolute inset-0 rounded-md cursor-pointer bg-secondary bg-opacity-70 flex gap-3 flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-all duration-300">
+      <h2 className="text-white text-[20px] font-medium">LONETE Kelly</h2>
+      <Image src={hr} width={100} height={1} alt='hr' className='-mt-2' />
+      <p className="text-white text-lg w-[70%]">Chief Executive Officer</p>
+      <div className='flex items-center gap-5'>
+        <Link href='#'><FaLinkedinIn   className='p-1 text-xl bg-[#0076B2] text-white'/></Link>
+        <Link href='#'><GrFacebookOption className='bg-[#1877F2] text-white text-2xl p-1 rounded-3xl' /></Link>
+        <Image src={ig} width={20} height={18} alt='instagram' />
+      </div>
+    </div>
+  </div>
+
+ 
+  <div className="relative group">
+    <Image src={Socrate} width={250} height={100} alt="kelly" className="object-cover transition-all duration-300 group-hover:opacity-50" />
+    <div className="absolute inset-0 rounded-md cursor-pointer bg-secondary bg-opacity-70 flex gap-3 flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-all duration-300">
+      <h2 className="text-white text-[20px] font-medium">AYIVUGWE Socrate</h2>
+      <Image src={hr} width={100} height={1} alt='hr' className='-mt-2' />
+      <p className="text-white text-lg w-[70%]">Hiring Manager</p>
+      <div className='flex items-center gap-5'>
+        <Link href='#'><FaLinkedinIn   className='p-1 text-xl bg-[#0076B2] text-white'/></Link>
+        <Link href='#'><GrFacebookOption className='bg-[#1877F2] text-white text-2xl p-1 rounded-3xl' /></Link>
+        <Image src={ig} width={20} height={18} alt='instagram' />
+      </div>
+    </div>
+  </div>
+ 
+  <div className="relative group">
+    <Image src={Henry} width={250} height={100} alt="kelly" className="object-cover transition-all duration-300 group-hover:opacity-50" />
+    <div className="absolute inset-0 rounded-md cursor-pointer bg-secondary bg-opacity-70 flex gap-3 flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-all duration-300">
+      <h2 className="text-white text-[20px] font-medium">HIRWA Heritier</h2>
+      <Image src={hr} width={100} height={1} alt='hr' className='-mt-2' />
+      <p className="text-white text-lg w-[70%]">Social Media Manager</p>
+      <div className='flex items-center gap-5'>
+        <Link href='#'><FaLinkedinIn   className='p-1 text-xl bg-[#0076B2] text-white'/></Link>
+        <Link href='#'><GrFacebookOption className='bg-[#1877F2] text-white text-2xl p-1 rounded-3xl' /></Link>
+        <Image src={ig} width={20} height={18} alt='instagram' />
+      </div>
+    </div>
+  </div>
+
+  <div className="relative group">
+    <Image src={Djasmine} width={250} height={100} alt="kelly" className="object-cover transition-all duration-300 group-hover:opacity-50" />
+    <div className="absolute inset-0 rounded-md cursor-pointer bg-secondary bg-opacity-70 flex gap-3 flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-all duration-300">
+      <h2 className="text-white text-[20px] font-medium">Keza Djasmine</h2>
+      <Image src={hr} width={100} height={1} alt='hr' className='-mt-2' />
+      <p className="text-white text-lg w-[70%]">Social Media Manager</p>
+      <div className='flex items-center gap-5'>
+        <Link href='#'><FaLinkedinIn   className='p-1 text-xl bg-[#0076B2] text-white'/></Link>
+        <Link href='#'><GrFacebookOption className='bg-[#1877F2] text-white text-2xl p-1 rounded-3xl' /></Link>
+        <Image src={ig} width={20} height={18} alt='instagram' />
+      </div>
+    </div>
+  </div>
+</div>
+
       </div>
     </div>
   );
