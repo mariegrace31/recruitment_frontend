b7e4854e5d4f7c93bc98731fba9a03197e65066f
mariegrace <mariegracebmg@gmail.com>
2024-12-05 23:30:50 +0200

    Fix mob version

--- app/components/Hr.jsx
+++ app/components/Hr.jsx
@@ -14,7 +14,7 @@ function Hr() {
       <div className='grid grid-cols-1 lg:grid-cols-3 gap-6 p-16 px-5 lg:px-32'>
 
         <div className='rounded-xl border border-black/20 flex flex-col gap-4 pb-6'>
-          <Image src={hrImage} width={377} height={100} alt='hr image' className='rounded-t-xl w-full object-cover' />
+          <Image src={hrImage} width={377} height={100} alt='hr image' className='rounded-t-xl w-full h-full object-cover' />
           <h3 className='px-3'>How to Prepare for a Job Interview</h3>
           <p className='px-3 text-[12px] font-light leading-6'>Preparing for a job interview can be daunting. Here are some practical tips to help you stand out and make a lasting impression on your potential employer...</p>
           <Link href='/hrdetail'>
@@ -23,7 +23,7 @@ function Hr() {
         </div>
 
          <div className='rounded-xl border border-black/20 flex flex-col gap-4 pb-6'>
-          <Image src={blog2} width={377} height={100} alt='hr image' className='rounded-t-xl w-full object-cover' />
+          <Image src={blog2} width={377} height={100} alt='hr image' className='rounded-t-xl w-full h-full object-cover' />
           <h3 className='px-3'>LK Recruitment embraces the agenda 2063 for a thriving future.</h3>
           <p className='px-3 text-[12px] font-light leading-6'>LK Recruitment Rwanda recently participated in the “One Africa, One HR” Summit, held at the Kigali Convention Center in Kigali, Rwanda.</p>
           <Link href='/blog2'>
